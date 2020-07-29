5be0e0943945101169509d97d8d4bef25f1a4fcb
piya03 <piyachaudhary50@gmail.com>
2020-07-29 23:43:46 +0530

    push the timer initial code

--- src/CommonButton.js
+++ src/CommonButton.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function CommonButton({ value, onClick }) {
+  console.log("value", { value });
   return (
     <div>
       <button className="btn" onClick={onClick}>
