6bcea4c45459022cd2be9bb8c861c34386b4e502
piya03 <piyachaudhary50@gmail.com>
2020-07-30 23:47:56 +0530

    added timer functionality

--- src/CommonButton.js
+++ src/CommonButton.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function CommonButton({ value, onClick }) {
-  console.log("value", { value });
   return (
     <div>
       <button className="btn" onClick={onClick}>
--- src/Timer.js
+++ src/Timer.js
@@ -3,6 +3,24 @@ import "./App.css";
 import CommonButton from "./CommonButton";
 
 function Timer() {
+  const [noOfSecond, setNoSeconds] = useState(60 * 60 * 48);
+  console.log("Timer -> noOfSecond", noOfSecond);
+  let hours = Math.floor(noOfSecond / (60 * 60));
+  console.log("Timer -> hours", hours);
+  let secondsLeft = noOfSecond % (60 * 60);
+  console.log("Timer -> secondsLeft", secondsLeft);
+  let minutes = Math.floor(secondsLeft / 60);
+  console.log("Timer -> minutes", minutes);
+  let seconds = secondsLeft % 60;
+  console.log("Timer -> seconds", seconds);
+
+  useEffect(() => {
+    let runTime = setTimeout(() => {
+      setNoSeconds((pre) => pre - 1);
+    }, 1000);
+
+    return () => clearTimeout(runTime);
+  }, [noOfSecond]);
   return (
     <>
       <div className="timer">
